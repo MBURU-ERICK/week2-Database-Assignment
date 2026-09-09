e4f66a5f21c6dcabe389425d7daf6be3a8315809
MBURU-ERICK <mburuerick29@gmail.com>
2026-09-09 22:37:23 +0300

    Update answer.sql

--- answer.sql
+++ answer.sql
@@ -1,1 +1,11 @@
 CREATE DATABASE sales;
+
+SELECT checkNumber,paymentDate,amount FROM payments;
+
+SELECT orderDate,requiredDate,status FROM orders WHERE status='In Process'ORDER BY orderDate DESC;
+
+SELECT firstName,lastName,email FROM employees WHERE jobtitle='Sales Rep'ORDER BY employeeNumber DESC;
+
+SELECT * FROM offices;
+
+SELECT productName,quantityInStock FROM products ORDER BY buyPrice ASC LIMIT 5;
